0a88d3083294934256d054276d47cd2be50ab840
Evans Acheampong <evansachie01@gmail.com>
2024-11-18 22:15:05 +0000

    update: use port for production

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from config import app
 from routes.whatsapp_routes import whatsapp_bp
+import os
 
 app.register_blueprint(whatsapp_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
